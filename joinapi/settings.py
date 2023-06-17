b9c8b8f56ad8dbc4fca067bcd1b26684e0f03a8c
niels-2005 <niels.scholz01@web.de>
2023-06-17 17:03:03 +0200

    csrf cookie secure

--- joinapi/settings.py
+++ joinapi/settings.py
@@ -41,6 +41,9 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = ["localhost", "127.0.0.1", "nielsscholz.pythonanywhere.com"]
 
+CSRF_COOKIE_SECURE = False
+
+
 # Application definition
 
 INSTALLED_APPS = [
