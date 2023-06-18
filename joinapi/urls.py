ebb0c45f871060da1f84182b2f7df3cd67be0d64
niels-2005 <niels.scholz01@web.de>
2023-06-18 13:04:22 +0200

    changed urls.py

--- joinapi/urls.py
+++ joinapi/urls.py
@@ -1,15 +1,27 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import admin
 from tasks.views import TaskViewSet
-
-router = DefaultRouter()
-router.register("tasks", TaskViewSet, basename="tasks")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("dj_rest_auth.urls")),
     path("api/registration/", include("dj_rest_auth.registration.urls")),
-    path("api/join/", include(router.urls)),
+    path(
+        "api/join/tasks/",
+        TaskViewSet.as_view({"get": "list", "post": "create"}),
+        name="tasks",
+    ),
+    path(
+        "api/join/tasks/<int:pk>/",
+        TaskViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="task-detail",
+    ),
 ]
